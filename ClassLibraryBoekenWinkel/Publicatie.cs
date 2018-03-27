1dc4c3cdfa090480f5efadd175f46635b63d598a
Luuk Hamming <luuk.hamming@gmail.com>
2018-03-27 11:38:58 +0200

    Added Abstract class Publicaties, added al the needed items to Boeken

---
+++ ClassLibraryBoekenWinkel/Publicatie.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryBoekenWinkel
+{
+    public abstract class Publicatie
+    {
+        private string titel;
+        private string auteur;
+        private string taal;
+
+        public string Titel
+        {
+            get
+            {
+                return titel;
+            }
+
+            set
+            {
+                titel = value;
+            }
+        }
+
+        public string Auteur
+        {
+            get
+            {
+                return auteur;
+            }
+
+            set
+            {
+                auteur = value;
+            }
+        }
+
+        public string Taal
+        {
+            get
+            {
+                return taal;
+            }
+
+            set
+            {
+                taal = value;
+            }
+        }
+
+
+    }
+}
