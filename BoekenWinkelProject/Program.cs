c38586f1a459793e883d84afc3e07fd8c54648e0
Felix Ruwe <0288704@student.rocvantwente.nl>
2018-03-30 20:20:05 +0200

    added refrence to class libary

--- BoekenWinkelProject/Program.cs
+++ BoekenWinkelProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibraryBoekenWinkel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace BoekenWinkelProject
     {
         static void Main(string[] args)
         {
+
             
         }
     }
