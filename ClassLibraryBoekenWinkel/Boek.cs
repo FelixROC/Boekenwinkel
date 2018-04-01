d2a797f2fa5b749f06ece0a3f3461bd5a530d871
Felix Ruwe <0288704@student.rocvantwente.nl>
2018-04-01 19:08:02 +0200

    Changed boeken and tijdschriften to publicaties

--- ClassLibraryBoekenWinkel/Boek.cs
+++ ClassLibraryBoekenWinkel/Boek.cs
@@ -19,51 +19,22 @@ namespace ClassLibraryBoekenWinkel
         private int Miniumaantal;
         private int Voorraad;
         private int Maxiumaantal;
-        #endregion
 
-        #region Boeken
-        Boek b1 = new Boek("Hercules", "Het oosten", 1, 3, 10);
-        Boek b2 = new Boek("Sneeuw Witje", "Het oosten", 2, 3, 10);
-        Boek b3 = new Boek("Hunger games", "Het oosten", 3, 3, 10);
-        Boek b4 = new Boek("IT", "Het oosten", 4, 3, 10);
-        #endregion
-
-        #region Methods
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Boek"/> class.
-        /// </summary>
-        /// <param name="titel">The titel.</param>
-        /// <param name="druk">The druk.</param>
-        /// <param name="iSBN">The i SBN.</param>
-        /// <param name="miniumaantal">The miniumaantal.</param>
-        /// <param name="maxiumaantal">The maxiumaantal.</param>
-        public Boek(string titel, string druk, int iSBN, int miniumaantal, int maxiumaantal)
+        public Boek(string druk, int iSBN, int miniumaantal, int maxiumaantal)
         {
-            this.titel = titel;
             this.druk = druk;
             this.iSBN = iSBN;
             Miniumaantal = miniumaantal;
             Maxiumaantal = maxiumaantal;
         }
 
-        /// <summary>
-        /// Publicaties the specified titel.
-        /// </summary>
-        /// <param name="_titel">The titel.</param>
-        /// <param name="_auteur">The auteur.</param>
-        /// <param name="_taal">The taal.</param>
-        /// <param name="_afmetingen">The afmetingen.</param>
-        /// <param name="_gewicht">The gewicht.</param>
-        /// <param name="_prijs">The prijs.</param>
-        public override void publicatie(string _titel, string _auteur, Enum_Taal _taal, Afmeting _afmetingen, int _gewicht, decimal _prijs)
+        public Boek(string _titel, string _auteur, string _taal, Enum _Taal, Afmeting _afmeting, int _gewicht, decimal _prijs) : base(_titel, _auteur, _taal, _Taal, _afmeting, _gewicht, _prijs)
         {
-            this.titel = _titel;
-            this.auteur = _auteur;
-            this.taal = _taal;
-            this.afmetingen = _afmetingen;
-            this.gewicht = _gewicht;
-            this.Prijs = _prijs;
         }
+        
+        #endregion
+
+        #region Methods
 
         public override string ToString()
         {
